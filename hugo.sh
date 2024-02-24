e78e8121832cf7b9705d8024724b834844b8e5d2
Heyxk <kevinhsiang@outlook.com>
2024-02-24 17:36:47 +0800

    update: 更换 hugo 镜像, 更新 hugo 版本

--- hugo.sh
+++ hugo.sh
@@ -1,1 +1,3 @@
-docker run --rm -it --user 1030:1030 -v $PWD:/src klakegg/hugo:0.95.0-ext-alpine $@
+#docker run --rm -it --user 1030:1030 -v $PWD:/src klakegg/hugo:0.112.4-ext-alpine $@
+cmd="hugo $@"
+docker run --rm -it --user 1030:1030 -v $PWD:/home/app betterweb/hugo:extended-0.121.1-20-1 -c "$cmd"
